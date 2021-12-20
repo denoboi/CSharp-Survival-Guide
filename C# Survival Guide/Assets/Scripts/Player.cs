2664d6c4d34e4671370468b5620c2bd423993878
denoboi <deniz.kaymakci@hotmail.com>
2021-12-20 15:57:36 +0300

    Custom Classes

---
+++ C# Survival Guide/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponStats
+{
+    public string name;
+    public float fireRate;
+    public int ammoCount;
+
+    public WeaponStats(string name, float fireRate, int ammoCount)
+    {
+        //Initialize things here
+        this.name = name;
+        this.fireRate = fireRate;
+        this.ammoCount = ammoCount;
+    }
+}
+
+
+
+public class Player : MonoBehaviour
+{
+    private WeaponStats blasters;
+    private WeaponStats rockets;
+
+    
+    void Start()
+    {
+        blasters = new WeaponStats("vurucu", 0.5f, 30) ; //bu yeni bir obje yaratiyor. simdi fieldlar ile kullanabiliriz.
+        rockets = new WeaponStats("Rockets",0.75f, 3);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
