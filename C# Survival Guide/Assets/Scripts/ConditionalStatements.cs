53c9e022c6d07aa5b00a5fb81e4205e13d600ac2
denoboi <deniz.kaymakci@hotmail.com>
2022-02-06 21:20:33 +0300

    Quiz Grades Average(Random.Range)

--- C# Survival Guide/Assets/Scripts/ConditionalStatements.cs
+++ C# Survival Guide/Assets/Scripts/ConditionalStatements.cs
@@ -4,17 +4,41 @@ using UnityEngine;
 
 public class ConditionalStatements : MonoBehaviour
 {
-    public int quiz1 = 90;
-    public int quiz2 = 90;
-    public int quiz3 = 44;
-    public int quiz4 = 69;
-    public int quiz5 = 88;
+    public int quiz1;
+    public int quiz2;
+    public int quiz3; 
+    public int quiz4; 
+    public int quiz5; 
 
     private float average;
+
     // Start is called before the first frame update
     void Start()
     {
-        average = quiz1 + quiz2 + quiz3 + quiz4 + quiz5 / 5;
+        quiz1 = Random.Range(0, 100);
+        quiz2 = Random.Range(0, 100);
+        quiz3 = Random.Range(0, 100);
+        quiz4 = Random.Range(0, 100);
+        quiz5 = Random.Range(0, 100);
+
+        average = (quiz1 + quiz2 + quiz3 + quiz4 + quiz5) / 5;
+
+        if(average > 90)
+        {
+            Debug.Log("Your grade is A: " + average);
+        }
+        else if(average >= 80 && average <= 90)
+        {
+            Debug.Log("Your grade is B: " + average);
+        }
+        else if(average >= 70 && average < 80)
+        {
+            Debug.Log("Your grade is C: " + average);
+        }
+        else
+        {
+            Debug.Log("You are idiot: " + average);
+        }
     }
 
     // Update is called once per frame
