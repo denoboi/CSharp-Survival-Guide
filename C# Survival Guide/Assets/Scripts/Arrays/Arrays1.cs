8a14bf287816a950aa6ca77530b317deda486282
denoboi <deniz.kaymakci@hotmail.com>
2022-02-12 23:53:24 +0300

    Arrays1

--- C# Survival Guide/Assets/Scripts/Arrays/Arrays1.cs
+++ C# Survival Guide/Assets/Scripts/Arrays/Arrays1.cs
@@ -8,6 +8,7 @@ public class Arrays1 : MonoBehaviour
     public string[] names = new string[] { "Ahmet", "Ali", "Ayse", "Denoboi", "Ezgi" };
     public int[] ages = new int[] { 5, 10, 54, 30, 29 };
     public string[] cars = new string[] { "Ford", "BMW", "Ferrari", "Koenigsegg", "Tofas" };
+
     
 
     // Start is called before the first frame update
@@ -23,7 +24,12 @@ public class Arrays1 : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log(" Name: " + names[4] + " Age: " + ages[4] + " Fav Car: " + cars[4]);
+
+            Debug.Log(names[Random.Range(0,names.Length)] + ages[4] + cars[4]);
+            
+            Debug.Log(cars[Random.Range(0, names.Length)]);
+
+
         }
     }
 }
